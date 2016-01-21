d1cffd6c99c8a4fcc787b6820574b98c2e819a3e
silvereh <silvere.heraudeau@gmail.com>
2016-01-21 12:01:30 -0700

    Added a few tweaks for command clarification for the player. Removed saving and loading functionalities which didn't work.

--- Adventure.py
+++ Adventure.py
@@ -10,38 +10,6 @@ import subprocess as sp
 import os
 import pickle
 
-GAMEFILE = "adventure.game"
-
-def loadGame():
-	"""This function loads the latest saved game. 
-	If the file doesn't exist, it starts a new game."""
-	if os.path.exists(GAMEFILE):
-		with open(GAMEFILE, 'rb') as gamefile:
-			unpic = pickle.Unpickler(gamefile)
-			game = unpic.load()
-	else:
-		game = Game()
-
-	return game
-def saveGame(game):
-	"""This function saves the current game in the defined game file.
-	It takes as a parameter the current game that we want to save."""
-	try:
-		with open(GAMEFILE, 'wb') as gamefile:
-			pic = pickle.Pickler(gamefile)
-			pic.dump(game)
-		print("The game was saved with success!")
-	except:
-		print("Oops ... Something went wrong and we couldn't save your game")
-
-load = save = ""
-load = raw_input("Hello, welcome in this adventure. Do you want to Load an existing game? ").lower()
-if load == "l":
-	print("Loading game ...")
-	game = loadGame()
-else:
-	game = Game()
-
 tmp = sp.call('clear',shell=True)
 game.help()
 print game.room.firstText
@@ -54,7 +22,5 @@ while x != "q":
 		game.command[x]()
 		x = raw_input()
 
-save = raw_input("So sad to see you leave. Do you want to Save? ").lower()
-if save == "s":
-	saveGame(game)
+print("So sad to see you leave.")
 exit(0)
